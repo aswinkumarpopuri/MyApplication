24a234775e5dbcaf9ab1871cfb304fa196514653
popuriaswinkumar <popuriaswinkumar@gmail.com>
2016-09-20 09:19:33 -0400

    Initial commit

---
+++ AndroidStudioProjects/MyApplication/app/src/main/java/com/mac/training/collectioner/TwitterSignInActivity.java
@@ -0,0 +1,13 @@
+package com.mac.training.collectioner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TwitterSignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_twitter_sign_in);
+    }
+}
